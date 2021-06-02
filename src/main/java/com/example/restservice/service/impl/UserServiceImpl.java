62c7104cc17ccab1a9aa16fa553e8f1c719645e0
ekochetkova <ekochetkova@diasoft.ru>
2021-06-02 14:13:23 +0300

    Added transactional

--- src/main/java/com/example/restservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/restservice/service/impl/UserServiceImpl.java
@@ -6,28 +6,34 @@ import com.example.restservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     @Autowired
     private  UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(int id) {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
     }
 
     @Override
+    @Transactional(rollbackFor = UserNotFoundException.class)
     public void deleteUserById(int id) {
-        userRepository.deleteById(id);
+       getUserById(id);
+       userRepository.deleteById(id);
     }
 
     @Override
@@ -36,9 +42,12 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(rollbackFor = UserNotFoundException.class)
     public void updateUser(User user) {
+        getUserById(user.getId());
         userRepository.save(user);
     }
+
     @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found in DB")
     public class UserNotFoundException extends RuntimeException {
         public UserNotFoundException(int id) {
