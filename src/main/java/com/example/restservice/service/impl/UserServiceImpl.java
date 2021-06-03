f880571af1984c9e0fb6934c4792edeb49cad5b5
ekochetkova <ekochetkova@diasoft.ru>
2021-06-03 16:17:18 +0300

    added db properties

--- src/main/java/com/example/restservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/restservice/service/impl/UserServiceImpl.java
@@ -6,12 +6,14 @@ import com.example.restservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     @Autowired
     private  UserRepository userRepository;
